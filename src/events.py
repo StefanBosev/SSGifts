c6fc8424672d0b4724cabd718759ec638aff0812
stephie306 <stefi.staykova306@gmail.com>
2021-06-01 16:44:18 +0300

    Admin Panel design and Event page
    Adding admin panel design and event page html to show new events

--- src/events.py
+++ src/events.py
@@ -1,34 +1,37 @@
 from src.database import DB
 
+
 class Event:
-    def __init__(id, event_end_date, name, price_low_border, price_high_border):
-        self.id = id
-        self.event_end_date = event_end_date
+    def __init__(self,  name, event_start_date, event_end_date,
+                 price_high_border, price_low_border):
         self.name = name
+        self.event_start_date = event_start_date
+        self.event_end_date = event_end_date
         self.price_high_border = price_high_border
         self.price_low_border = price_low_border
-
+    
     def create(self):
         with DB() as db:
-            values = (self.name, self.event_end_date, self.price_high_border, self.price_low_border)
+            values = (self.name, self.event_start_date, self.event_end_date,
+                      self.price_high_border, self.price_low_border)
             db.execute('''
-                INSERT INTO Event(name, event_end_date, price_high_border, price_low_border) VALUES(?, ?, ?, ?)
+                INSERT INTO Event(name, event_start_date, event_end_date, price_high_border, price_low_border) VALUES(?, ?, ?, ?, ?)
                 ''', values)
-
+            
             return self
-
+    
     def delete(self):
         with DB() as db:
             db.execute('''
                 DELETE FROM Event WHERE event_id = ?
                 ''', self.id)
-
+    
     @staticmethod
     def all():
         with DB() as db:
             all = db.execute('SELECT * FROM Event').fetchall()
-            return [Event(*one) for one in all] 
-
+            return all
+        
     @staticmethod
     def find_by_id(id):
         with DB() as db:
@@ -36,8 +39,8 @@ class Event:
                 SELECT id, event_end_date, name, price_low_border, price_high_border FROM Event
                 WHERE event.id = ?
             ''', (id,)).fetchone()
-
+            
             if values:
                 return Event(*values)
-
+            
             return None
